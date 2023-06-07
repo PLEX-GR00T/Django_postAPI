5f33a2a309965d3bab08592ec8c8be28d18ae10a
Priyank Thakkar <pthakkar16298@gmail.com>
2023-06-06 23:39:08 -0700

    Changed API keys with Variable (.env file)

--- users/tasks.py
+++ users/tasks.py
@@ -3,6 +3,14 @@ import json
 from datetime import datetime
 from django.contrib.auth import get_user_model
 from time import sleep
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GEO_KEY = os.getenv('GEO_KEY')
+TZ_KEY = os.getenv('TZ_KEY')
+HLD_KEY = os.getenv('HLD_KEY')
+
 
 def enrich_user_data(user_id):
     # sleep(30)
@@ -10,17 +18,17 @@ def enrich_user_data(user_id):
     user = User.objects.get(id = user_id)
 
     # Geo location (Todo: Handle API exceptions, move keys to env file,)
-    response = json.loads(requests.get(f"https://ipgeolocation.abstractapi.com/v1/?api_key=fcc203c58fa14456839d1646a45107b8&ip_address={user.signup_ip}").content)
+    response = json.loads(requests.get(f"https://ipgeolocation.abstractapi.com/v1/?api_key={GEO_KEY}&ip_address={user.signup_ip}").content)
     user.signup_location = f"{response['city']};{response['region']};{response['country']}"
     user.save()
 
     # Time Zone
-    timezone = json.loads(requests.get(f'https://timezone.abstractapi.com/v1/current_time/?api_key=c053f763e49a4f59be1810a1f73322de&location="{user.signup_ip}"').content)
+    timezone = json.loads(requests.get(f'https://timezone.abstractapi.com/v1/current_time/?api_key={TZ_KEY}&location="{user.signup_ip}"').content)
     # print(timezone)
     dt = datetime.fromisoformat(timezone['datetime'])
     
     # Is holiday
-    holiday = json.loads(requests.get(f"https://holidays.abstractapi.com/v1/?api_key=f688711adcb44155ae10450710577340\
+    holiday = json.loads(requests.get(f"https://holidays.abstractapi.com/v1/?api_key={HLD_KEY}\
                                         &country={response['country_code']}&year={dt.year}&month={dt.month}&day={dt.day}").content)
 
     if holiday:
